aa1555a274c8e2d50a6448de660a11acc25af1fc
Jimmy Luna <jimmylunad@gmail.com>
2022-04-15 22:03:09 -0500

    creación de metodos http

---
+++ src/task/task.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { TaskDTO } from './dto/task.dto';
+import { ITask } from './task.interface';
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable()
+export class TaskService {
+  tasks: ITask[] = [];
+  create(taskDTO: TaskDTO): ITask {
+    const task = {
+      id: uuidv4(),
+      ...taskDTO,
+    };
+    this.tasks.push(task);
+    return task;
+  }
+
+  findAll(): ITask[] {
+    return this.tasks;
+  }
+
+  findOne(id: string): ITask {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  update(id: string, taskDTO: TaskDTO): ITask {
+    const newTask = { id, ...taskDTO };
+    this.tasks = this.tasks.map((task) => (task.id === id ? newTask : task));
+    return newTask;
+  }
+
+  delete(id: string): string {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+    return `task #${id} deleted`;
+  }
+}
